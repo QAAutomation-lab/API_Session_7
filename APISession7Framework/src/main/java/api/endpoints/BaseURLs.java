0f14db6feb96e41dd037ca4654ae91d2df9676c7
QAAutomation-lab <automationguide13@gmail.com>
2025-04-05 19:59:35 +0530

    Framework Structure

---
+++ APISession7Framework/src/main/java/api/endpoints/BaseURLs.java
@@ -0,0 +1,16 @@
+package api.endpoints;
+
+public class BaseURLs {
+	//Base URL
+	public static final String BASE_URL="https://petstore.swagger.io/v2";
+	//Target URLs for USER flow
+	public static final String CREATE_USER_URL=BASE_URL+"/user";
+	public static final String GET_USER_URL=BASE_URL+"/user/{username}";
+	public static final String UPDATE_USER_URL=BASE_URL+"/user/{username}";
+	public static final String DETELE_USER_URL=BASE_URL+"/user/{username}";
+	
+	//Target URLs for Store flow
+	
+	
+	//Target URLs for PET flow
+}
